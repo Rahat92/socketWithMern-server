7c28854e8d5c784eb64a5479ff01a9f4987891ae
khrahat92 <khrahat92@gmail.com>
2023-06-24 23:33:58 +0600

    change cors origin

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 });
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socketlivevideoserver.onrender.com/",
     methods: ["GET", "POST"],
   },
 });

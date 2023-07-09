2af4b513933bf5660c2b1507dbbfee15bae5ff31
khrahat92 <khrahat92@gmail.com>
2023-07-09 13:28:28 +0600

    change /

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://socketlivevideoserver.onrender.com/"
+    "https://socketlivevideoserver.onrender.com"
   );
 
   // Request methods you wish to allow
